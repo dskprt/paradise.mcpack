57f165ac6221163f72cccac117cfe42f39304d0c
dskprt <59660013+dskprt@users.noreply.github.com>
2025-03-17 17:11:41 +0100

    Revert "a"
    This reverts commit c27cc00c4b6bf3b9666cfb151c9f26a770ae9ae1.

--- minecraft/kubejs/server_scripts/ore_processing_ie.js
+++ minecraft/kubejs/server_scripts/ore_processing_ie.js
@@ -3,7 +3,7 @@ ServerEvents.recipes(e => {
 	function r(e, ore, oredrop, ir, clr, crr, r) {
 		// remove recipes for ingot
 		//e.remove({ output: r })
-		e.remove({ input: ore, output: r })
+		/*e.remove({ input: ore, output: r })
 		e.remove({ input: ore.replace(":", ":deepslate_"), output: r })
 		e.remove({ input: ore, output: "#forge:ingots/" + ir })
 		e.remove({ input: ore.replace(":", ":deepslate_"), output: "#forge:ingots/" + ir })
@@ -15,12 +15,7 @@ ServerEvents.recipes(e => {
 		e.remove({ input: "#forge:raw_materials/" + clr, output: "#forge:ingots/" + ir })
 
 		e.remove({ id: "immersiveengineering:arcfurnace/raw_block_" + ir })
-		e.remove({ id: "immersiveengineering:arcfurnace/raw_ore_" + ir })
-
-		e.remove({ id: "tconstruct:smeltery/melting/metal/" + ir + "/ore_dense" })
-		e.remove({ id: "tconstruct:smeltery/melting/metal/" + ir + "/ore_singular" })
-		e.remove({ id: "tconstruct:smeltery/melting/metal/" + ir + "/raw_ore" })
-		e.remove({ id: "tconstruct:smeltery/melting/metal/" + ir + "/raw_block" })
+		e.remove({ id: "immersiveengineering:arcfurnace/raw_ore_" + ir })*/
 		
 		// crushing recipes for raw ore block to raw ore
 		e.recipes.create.crushing(oredrop, ore)
