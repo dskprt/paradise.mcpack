2e181a18818c85e099ec998b3d5b947ce345c4c6
dskprt <59660013+dskprt@users.noreply.github.com>
2025-03-22 22:20:47 +0100

    Update fixes.js

--- minecraft/kubejs/server_scripts/fixes.js
+++ minecraft/kubejs/server_scripts/fixes.js
@@ -10,6 +10,6 @@ ServerEvents.recipes(e => {
 	e.shapeless("createaddition:zinc_sheet", ["#immersiveengineering:tools/hammers", "#forge:ingots/zinc"])
 
 	e.replaceInput({ id: "toms_storage:storage_terminal" }, "glowstone", "redstone_block")
-	e.replaceInput({ id: "toms_storage:storage_terminal", "comparator", "repeater" })
-	e.replaceInput({ id: "toms_storage:inventory_connector", "comparator", "repeater" })
+	e.replaceInput({ id: "toms_storage:storage_terminal" }, "comparator", "repeater")
+	e.replaceInput({ id: "toms_storage:inventory_connector" }, "comparator", "repeater")
 })
