02cf296f4b9009f0fb9180042a8a7ba314673974
dskprt <59660013+dskprt@users.noreply.github.com>
2025-03-22 22:10:25 +0100

    aaaa

--- minecraft/kubejs/server_scripts/fixes.js
+++ minecraft/kubejs/server_scripts/fixes.js
@@ -9,5 +9,5 @@ ServerEvents.recipes(e => {
 
 	e.shapeless("createaddition:zinc_sheet", ["#immersiveengineering:tools/hammers", "#forge:ingots/zinc"])
 
-	e.replaceInput({ id: "toms_storage:storage_terminal", "glowstone", "redstone_block" })
+	e.replaceInput({ id: "toms_storage:storage_terminal" }, "glowstone", "redstone_block" })
 })
