f12d35517c3447705af0789f2418ada240e0cfd5
dskprt <59660013+dskprt@users.noreply.github.com>
2025-04-02 19:03:12 +0200

    fixz coins

--- minecraft/kubejs/server_scripts/research.js
+++ minecraft/kubejs/server_scripts/research.js
@@ -24,10 +24,10 @@ ServerEvents.recipes(e => {
 	e.recipes.create.sequenced_assembly([
 		Item.of("kubejs:chemical_coin").withChance(100),
 	], "kubejs:coin", [
-		e.recipes.create.deploying(inter, [ inter, "#forge:plates/brass" ]),
+		e.recipes.create.deploying(inter, [ inter, "chemlib:hydrogen" ]),
 		e.recipes.create.deploying(inter, [ inter, "#forge:gems/diamond" ]),
 		e.recipes.create.cutting(inter, inter),
-		e.recipes.create.deploying(inter, [ inter, "chemlib:hydrogen" ]),
+		e.recipes.create.deploying(inter, [ inter, "#forge:plates/brass" ]),
 		e.recipes.create.pressing(inter, inter),
 		e.recipes.create.cutting(inter, inter),
 	]).transitionalItem(inter).loops(4)
@@ -36,10 +36,10 @@ ServerEvents.recipes(e => {
 	e.recipes.create.sequenced_assembly([
 		Item.of("kubejs:electronics_coin").withChance(100),
 	], "kubejs:coin", [
-		e.recipes.create.deploying(inter, [ inter, "#forge:plates/brass" ]),
+		e.recipes.create.deploying(inter, [ inter, "create_new_age:copper_circuit" ]),
 		e.recipes.create.deploying(inter, [ inter, "#forge:gems/diamond" ]),
 		e.recipes.create.cutting(inter, inter),
-		e.recipes.create.deploying(inter, [ inter, "create_new_age:copper_circuit" ]),
+		e.recipes.create.deploying(inter, [ inter, "#forge:plates/brass" ]),
 		e.recipes.create.pressing(inter, inter),
 		e.recipes.create.cutting(inter, inter),
 	]).transitionalItem(inter).loops(3)
@@ -48,11 +48,11 @@ ServerEvents.recipes(e => {
 	e.recipes.create.sequenced_assembly([
 		Item.of("kubejs:engineering_coin").withChance(100),
 	], "kubejs:coin", [
-		e.recipes.create.deploying(inter, [ inter, "#forge:plates/brass" ]),
+		e.recipes.create.deploying(inter, [ inter, "#forge:plates/lead" ]),
 		e.recipes.create.deploying(inter, [ inter, "#forge:gems/diamond" ]),
 		e.recipes.create.cutting(inter, inter),
-		e.recipes.create.deploying(inter, [ inter, "#forge:plates/lead" ]),
 		e.recipes.create.deploying(inter, [ inter, "#forge:plates/steel" ]),
+		e.recipes.create.deploying(inter, [ inter, "#forge:plates/brass" ]),
 		e.recipes.create.pressing(inter, inter),
 		e.recipes.create.cutting(inter, inter),
 	]).transitionalItem(inter).loops(4)
@@ -61,11 +61,11 @@ ServerEvents.recipes(e => {
 	e.recipes.create.sequenced_assembly([
 		Item.of("kubejs:logistics_coin").withChance(100),
 	], "kubejs:coin", [
-		e.recipes.create.deploying(inter, [ inter, "#forge:plates/brass" ]),
+		e.recipes.create.deploying(inter, [ inter, "#forge:barrels" ]),
 		e.recipes.create.deploying(inter, [ inter, "#forge:gems/diamond" ]),
 		e.recipes.create.cutting(inter, inter),
-		e.recipes.create.deploying(inter, [ inter, "#forge:barrels" ]),
 		e.recipes.create.deploying(inter, [ inter, "#forge:slimeballs" ]),
+		e.recipes.create.deploying(inter, [ inter, "#forge:plates/brass" ]),
 		e.recipes.create.pressing(inter, inter),
 		e.recipes.create.cutting(inter, inter),
 	]).transitionalItem(inter).loops(5)
@@ -74,11 +74,11 @@ ServerEvents.recipes(e => {
 	e.recipes.create.sequenced_assembly([
 		Item.of("kubejs:science_coin").withChance(100),
 	], "kubejs:engineering_coin", [
-		e.recipes.create.deploying(inter, [ inter, "#forge:plates/brass" ]),
+		e.recipes.create.deploying(inter, [ inter, "#forge:dusts/fissile" ]),
 		e.recipes.create.deploying(inter, [ inter, "#forge:gems/diamond" ]),
 		e.recipes.create.cutting(inter, inter),
 		e.recipes.create.deploying(inter, [ inter, "#forge:ender_pearls" ]),
-		e.recipes.create.deploying(inter, [ inter, "#forge:dusts/fissile" ]),
+		e.recipes.create.deploying(inter, [ inter, "#forge:plates/brass" ]),
 		e.recipes.create.pressing(inter, inter),
 		e.recipes.create.cutting(inter, inter),
 	]).transitionalItem(inter).loops(7)
@@ -87,11 +87,11 @@ ServerEvents.recipes(e => {
 	e.recipes.create.sequenced_assembly([
 		Item.of("kubejs:combat_coin").withChance(100),
 	], "kubejs:coin", [
-		e.recipes.create.deploying(inter, [ inter, "#forge:plates/brass" ]),
+		e.recipes.create.deploying(inter, [ inter, "rotten_flesh" ]),
 		e.recipes.create.deploying(inter, [ inter, "#forge:gems/diamond" ]),
 		e.recipes.create.cutting(inter, inter),
-		e.recipes.create.deploying(inter, [ inter, "rotten_flesh" ]),
 		e.recipes.create.deploying(inter, [ inter, "experience_bottle" ]),
+		e.recipes.create.deploying(inter, [ inter, "#forge:plates/brass" ]),
 		e.recipes.create.pressing(inter, inter),
 	]).transitionalItem(inter).loops(3)
 })
